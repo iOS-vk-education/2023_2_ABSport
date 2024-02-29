0111b3142ebaba27929e9db2e3810212e741b1cb
Egor Ivanov <101597786+Ozondo@users.noreply.github.com>
2024-02-29 14:48:26 +0300

    start Coordinator

---
+++ ABSport/ABSport/Auth/Registration/RegistrationViewController.swift
@@ -0,0 +1,19 @@
+//
+//  RegistrationViewController.swift
+//  ABSport
+//
+//  Created by Егор Иванов on 29.02.2024.
+//
+
+import UIKit
+
+class RegistrationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .green
+
+        // Do any additional setup after loading the view.
+    }
+
+}
---
+++ ABSport/ABSport/Navigation/CoordinatorProtocol .swift
@@ -0,0 +1,17 @@
+//
+//  MainCoordinator .swift
+//  ABSport
+//
+//  Created by Егор Иванов on 29.02.2024.
+//
+
+import UIKit
+
+// MARK: - coordinator protocol (от него наследуются все координаторы) 
+protocol Coordinator: AnyObject {
+    var childCoodinator: [Coordinator] { get }
+    
+    func start()
+    func addDependency(_ coordinator: Coordinator)
+    func removeDependency(_ coordinator: Coordinator)
+}
