2e4b013f4ee6a3559cc3a0fbd0d8d21532831d34
Apollonoff <apollonoff.arseniy@gmail.com>
2024-03-02 23:04:35 +0300

    Fix naming, delete storyboards

--- ABSport/ABSport/Main/MainCoordinator.swift
+++ ABSport/ABSport/Main/MainCoordinator.swift
@@ -5,8 +5,29 @@
 //  Created by Arseniy Apollonov on 02.03.2024.
 //
 
-import Foundation
+import UIKit
 
-class MainCoordinator {
+class MainCoordinator: Coordinator {
+    
+    var childCoodinator: [Coordinator] = []
+    
+    private let navigationController: UINavigationController
+    
+    func start() {
+        //
+    }
+    
+    func addDependency(_ coordinator: Coordinator) {
+        childCoodinator.append(coordinator)
+    }
+    
+    func removeDependency(_ coordinator: Coordinator) {
+        guard let index = childCoodinator.firstIndex(where: { coordinator === $0}) else { return }
+        childCoodinator.remove(at: index)
+    }
+    
+    init(navigationController: UINavigationController) {
+        self.navigationController = navigationController
+    }
     
 }
--- ABSport/ABSport/SceneDelegate.swift
+++ ABSport/ABSport/SceneDelegate.swift
@@ -9,15 +9,15 @@ import UIKit
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     
-    private var mainCoordinator: MainCoordinator?
+    private var appCoordinator: AppCoordinator?
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         let window = UIWindow(windowScene: windowScene)
         
-        mainCoordinator = MainCoordinator(window: window)
+        appCoordinator = AppCoordinator(window: window)
         
-        mainCoordinator?.start()
+        appCoordinator?.start()
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
